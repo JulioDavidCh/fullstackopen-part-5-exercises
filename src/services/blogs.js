2c3de1d78c904c11d20f443e551f4fdac4c7e05b
Julio <juliodavidchopite@gmail.com>
2019-12-22 19:04:43 -0300

    extended our blog services to include a method to add new blogs and set the token for the requests

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,22 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
+let token = ''
 
-const getAll = () => {
+const setToken = userToken =>{
+  token = ` bearer ${userToken.token}`
+}
+
+const getAll = () =>{
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll }
+const createBlog = async newBlog =>{
+  const config = {
+    headers: {Authorization: token},
+  }
+  const request = await axios.post(baseUrl, newBlog, config)
+  return request.data
+}
+
+export default { getAll, setToken, createBlog }

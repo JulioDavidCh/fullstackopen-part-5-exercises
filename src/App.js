26f380682d3756a87a1559d8de6a5fa78857352e
Julio <juliodavidchopite@gmail.com>
2019-12-22 16:10:39 -0300

    extended the app to allow users to keep sessions and logout

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
-import React,{ useState} from 'react';
+import React,{ useState, useEffect} from 'react';
 import loginService from './services/login'
+import blogsService from './services/blogs'
 import DisplayBlog from './components/Blog'
 
 function App() {
   const [userName, setUserName] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [blogList, setBloglist] = useState([])
 
-  const BlogList  = [
-    {
-      title: "testing",
-      author: "testing",
-      url: "testing",
-      likes: 5,
-      id: "5df17a40f9726b3c70fa2874"
-    },
-    {
-      title: "probando211",
-      author: "probando211",
-      url: "probando211",
-      likes: 0,
-      user: {
-      username: "gab",
-      name: "julio",
-      id: "5dfcd853da466138accb26bd"
-    },
-      id: "5dfd3ab45a6d3826847fe7a1"
+  // const BlogList  = [
+  //   {
+  //     title: "testing",
+  //     author: "testing",
+  //     url: "testing",
+  //     likes: 5,
+  //     id: "5df17a40f9726b3c70fa2874"
+  //   },
+  //   {
+  //     title: "probando211",
+  //     author: "probando211",
+  //     url: "probando211",
+  //     likes: 0,
+  //     user: {
+  //     username: "gab",
+  //     name: "julio",
+  //     id: "5dfcd853da466138accb26bd"
+  //   },
+  //     id: "5dfd3ab45a6d3826847fe7a1"
+  //   }
+  // ]
+  
+  useEffect(()=>{
+    const helper = async()=>{
+      const storedUser = window.localStorage.getItem('loggedUser')
+      if(storedUser) setUser(JSON.parse(storedUser))
+      const blogs = await blogsService.getAll()
+      setBloglist(blogs)
     }
-    ]
+    helper()
+  },[])
 
   const loginHandler = async (event) => {
     event.preventDefault()
@@ -37,6 +49,9 @@ function App() {
     }
 
     const userData = await loginService.login(credentials)
+
+    window.localStorage.setItem('loggedUser', JSON.stringify(userData))
+
     setUser(userData)
     console.log(userData)
   }
@@ -68,12 +83,23 @@ function App() {
     </div>
   )
 
+  const logoutHandler = event => {
+    //event.preventDefault()
+    setUser(null)
+    window.localStorage.removeItem('loggedUser')
+  }
+
   const displayBlogs = () => (
     <div>
-      <h2>
-        Blogs:
-      </h2>
-      {BlogList.map(blog => <DisplayBlog key={blog.id} blog={blog} />)}
+      <div>
+        <h2>
+          Blogs:
+        </h2>
+        {user.name} logged in
+        <button onClick={logoutHandler} >logout</button>
+      </div>
+      <br></br>
+      {blogList.map(blog => <DisplayBlog key={blog.id} blog={blog} />)}
     </div>
   )
   return (

2e313dcdce9cccaa6bce31626d1bdea6beaaea7e
Julio <juliodavidchopite@gmail.com>
2019-12-22 09:42:15 -0300

    initial push

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React,{ useState} from 'react';
+import loginService from './services/login'
+import DisplayBlog from './components/Blog'
+
+function App() {
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const BlogList  = [
+    {
+      title: "testing",
+      author: "testing",
+      url: "testing",
+      likes: 5,
+      id: "5df17a40f9726b3c70fa2874"
+    },
+    {
+      title: "probando211",
+      author: "probando211",
+      url: "probando211",
+      likes: 0,
+      user: {
+      username: "gab",
+      name: "julio",
+      id: "5dfcd853da466138accb26bd"
+    },
+      id: "5dfd3ab45a6d3826847fe7a1"
+    }
+    ]
+
+  const loginHandler = async (event) => {
+    event.preventDefault()
+    const credentials = {
+      username: userName,
+      password
+    }
+
+    const userData = await loginService.login(credentials)
+    setUser(userData)
+    console.log(userData)
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={loginHandler} >
+        <div>
+          Username: 
+          <input
+          type="text"
+          value={userName}
+          name="Username"
+          onChange={({target}) => setUserName(target.value) }
+          />
+        </div> 
+        <div>
+          Password:
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({target}) => setPassword(target.value) }
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+
+  const displayBlogs = () => (
+    <div>
+      <h2>
+        Blogs:
+      </h2>
+      {BlogList.map(blog => <DisplayBlog key={blog.id} blog={blog} />)}
+    </div>
+  )
+  return (
+    <div>
+      {
+        user === null
+        ? loginForm()
+        : displayBlogs()
+      }
+    </div>
+  )
+}
+
+export default App;

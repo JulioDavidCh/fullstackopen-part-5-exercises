36a2b059a3848f2a27a0c0f9eab85d553ccbcea6
Julio <juliodavidchopite@gmail.com>
2019-12-25 15:22:56 -0300

    extended it so that it shows and hide the whole blog when the user clicks it

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React,{ useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blogData }) => {
+  const [blog, setBlog] = useState('')
+  let showWholeBlog = false
+
+  let onClickHideHandler = () => {
+    if(showWholeBlog) setBlog(completeBlog)
+    if(!showWholeBlog) setBlog('')
+    showWholeBlog = !showWholeBlog
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const completeBlog = (
+    <div>
+      <br></br>
+      <a href={blogData.url}>{blogData.url}</a>
+      <p>likes: {blogData.likes}  <button onClick={event => event.stopPropagation()}>Like</button></p>
+      <p>added by {blogData.user.username}</p>
+    </div>
+  )
+
+  return (
+    <div onClick={onClickHideHandler} style={blogStyle}>
+      {blogData.title} {blogData.author}
+      {blog}
+    </div>
+  )
+}
 
 export default Blog

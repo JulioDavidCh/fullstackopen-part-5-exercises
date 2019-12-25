4e668ae0d5b23220ec2452b1844ca9a849b3e774
Julio <juliodavidchopite@gmail.com>
2019-12-25 20:36:31 -0300

    implemented a like system for blogs

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,13 +1,32 @@
 import React,{ useState } from 'react'
+import BlogService from '../services/blogs'
 
 const Blog = ({ blogData }) => {
   const [blog, setBlog] = useState('')
   let showWholeBlog = false
+  const [currentBlog, setCurrentBlog] = useState(blogData)
 
-  let onClickHideHandler = () => {
+  const {title, author, url, likes, id} = currentBlog
+
+  const onClickHideHandler = () => {
     if(showWholeBlog) setBlog(completeBlog)
     if(!showWholeBlog) setBlog('')
     showWholeBlog = !showWholeBlog
+  }
+
+  const onLikeHandler = async event => {
+
+    //event.stopPropagation()
+
+    const likedBlog = {
+      title,
+      author,
+      url,
+      likes: likes + 1,
+      id
+    }
+    const updatedBlog = await BlogService.updateBlog(likedBlog)
+    setCurrentBlog(updatedBlog)
   }
 
   const blogStyle = {
@@ -17,19 +36,18 @@ const Blog = ({ blogData }) => {
     borderWidth: 1,
     marginBottom: 5
   }
-
   const completeBlog = (
     <div>
       <br></br>
-      <a href={blogData.url}>{blogData.url}</a>
-      <p>likes: {blogData.likes}  <button onClick={event => event.stopPropagation()}>Like</button></p>
-      <p>added by {blogData.user.username}</p>
+      <a href={currentBlog.url}>{currentBlog.url}</a>
+      <p>likes: {currentBlog.likes}  <button onClick={onLikeHandler}>Like</button></p>
+      <p>added by {currentBlog.user.username}</p>
     </div>
   )
 
   return (
     <div onClick={onClickHideHandler} style={blogStyle}>
-      {blogData.title} {blogData.author}
+      {currentBlog.title} {currentBlog.author}
       {blog}
     </div>
   )

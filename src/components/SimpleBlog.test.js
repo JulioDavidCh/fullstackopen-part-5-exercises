ae61c043969af7f675620ad8c6486891f697451e
Julio <juliodavidchopite@gmail.com>
2019-12-29 17:38:24 -0300

    refactored the render test and added a test to check for clicks

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -1,36 +1,47 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 
-const blog = {
+const newBlog = {
   title: 'testing',
   author: 'john',
   likes: 1
 }
 
-test('renders title', () => {
+let component
 
-  const component = render(
-    <SimpleBlog blog={blog} />
-  )
+describe('Blog is correctly rendered for', () => {
 
-  expect(component.container).toHaveTextContent('testing')
+  beforeEach(() => {
+    component = render(
+      <SimpleBlog blog={newBlog} />
+    )
+  })
+
+  test('title', () => {
+    expect(component.container).toHaveTextContent('testing')
+  })
+
+  test('author', () => {
+    expect(component.container).toHaveTextContent('john')
+  })
+
+  test('likes', () => {
+    expect(component.container).toHaveTextContent('1')
+  })
+
 })
 
-test('renders author', () => {
+test('button is properly pressed twice', () => {
+  const onClickMock = jest.fn()
 
-  const component = render(
-    <SimpleBlog blog={blog} />
+  component = render(
+    <SimpleBlog blog={newBlog} onClick={onClickMock} />
   )
 
-  expect(component.container).toHaveTextContent('john')
-})
+  const button = component.container.querySelector('button')
+  fireEvent.click(button)
+  fireEvent.click(button)
 
-test('renders likes', () => {
-
-  const component = render(
-    <SimpleBlog blog={blog} />
-  )
-
-  expect(component.container).toHaveTextContent('1')
+  expect(onClickMock.mock.calls.length).toBe(2)
 })
